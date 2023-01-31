f443efe6cfcb68a05fa46c5360a2898654e223ef
borhez <getlink32@gmail.com>
2023-01-31 09:03:29 +0300

    last boot

--- linux-monitoring.sh
+++ linux-monitoring.sh
@@ -8,4 +8,5 @@ vCPU: $(nproc --all)
 Memory Usage: $(free -m | grep "Mem:" | awk '{printf($3"/"$2"Mb ""(%.2f%)"), $3/$2*100}')
 
 CPU load: $(top -b -n 1 | awk 'NR>7 {cpu=cpu+$9} END {printf("%.1f%%", cpu)}')
+Last boot: $(who -b | awk '{print $3 " " $4}')
 "
