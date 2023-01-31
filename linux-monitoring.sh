24d9a02024900f95b2d6c5d34b0eee4cb86ea3f8
borhez <getlink32@gmail.com>
2023-01-31 08:59:16 +0300

    cpu load

--- linux-monitoring.sh
+++ linux-monitoring.sh
@@ -6,4 +6,6 @@ Architecture: $(uname -a)
 CPU physical: $(lscpu | grep "Socket(s):" | awk '{print $2}')
 vCPU: $(nproc --all)
 Memory Usage: $(free -m | grep "Mem:" | awk '{printf($3"/"$2"Mb ""(%.2f%)"), $3/$2*100}')
+
+CPU load: $(top -b -n 1 | awk 'NR>7 {cpu=cpu+$9} END {printf("%.1f%%", cpu)}')
 "
